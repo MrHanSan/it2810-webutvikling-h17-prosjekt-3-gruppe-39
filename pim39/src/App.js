af71f5f394ec1bbb9b6b064ed21f0f5600ac7d4a
DeimosA <krvolden@gmail.com>
2017-10-01 20:30:53 +0200

    Customize header and responsive

--- pim39/src/App.js
+++ pim39/src/App.js
@@ -1,26 +1,40 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './39logo.svg';
 import './App.css';
 
 import TodoApp from './todo/TodoApp.js';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Personal Information Manager No. 39</h1>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div className="todoAppCont">
-            <TodoApp />
-        </div>
-      </div>
-    );
-  }
+class Header extends Component {
+    render() {
+        return (
+            <div id="pim_header">
+                <div id="logo_title">
+                    <img src={logo} id="pim_logo" alt="PIM39 logo" />
+                    <h1><span>Personal Information Manager No. 39</span></h1>
+                </div>
+                <nav id="navbar">
+                    <button type="button" className="active_button">Calendar</button>
+                    <button type="button" id="todo_button">To-do</button>
+                    <button type="button">Notes</button>
+                </nav>
+            </div>
+        );
+    }
 }
 
-export default App;
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <p className="App-intro">
+                    kaaaaalender
+                </p>
+                <div className="todoAppCont">
+                    <TodoApp />
+                </div>
+            </div>
+        );
+    }
+}
+
+export {App as default, Header};
--- pim39/src/index.js
+++ pim39/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App, {Header} from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 // testing
@@ -9,6 +9,6 @@ import registerServiceWorker from './registerServiceWorker';
 //import TodoApp from './todo/TodoApp.js';
 //ReactDOM.render(<TodoApp />, document.getElementById('root'));
 
-
+ReactDOM.render(<Header />, document.getElementById('header'));
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
