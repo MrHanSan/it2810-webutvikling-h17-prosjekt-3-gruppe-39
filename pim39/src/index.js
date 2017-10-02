f75e3ca630533671378b17315883e611615bacae
weiwei18w@hotmail.com <weiwei18w@hotmail.com>
2017-10-02 14:15:24 +0200

    try something

--- pim39/src/index.js
+++ pim39/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App, {Header} from './App';
 import registerServiceWorker from './registerServiceWorker';
+//import moment from "moment";//
 
-// testing
+
 
 //import TodoApp from './todo/TodoApp.js';
 //ReactDOM.render(<TodoApp />, document.getElementById('root'));
+
+
 
 ReactDOM.render(<Header />, document.getElementById('header'));
 ReactDOM.render(<App />, document.getElementById('root'));
